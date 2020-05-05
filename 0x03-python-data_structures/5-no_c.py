3ceba26adb19d00250aedbcab281029bbfd7b302
Ang3l1t0 <1546@holbertonschool.com>
2020-05-05 17:08:19 -0500

    no C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    return(my_string.translate({ord(i): None for i in 'cC'}))
+    str = ""
+    for i in my_string:
+        if (i != 'c' and i != 'C'):
+            str = str + i
+    return(str)
