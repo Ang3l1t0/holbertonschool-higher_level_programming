7c406013187c5946445683d3c0f623c91381baee
Ang3l1t0 <1546@holbertonschool.com>
2020-05-18 12:43:53 -0500

    variable out of for

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    printed = 0
     for i in range(x):
-        printed = 0
         try:
             print(my_list[i], end="")
             printed += 1
         except IndexError:
             pass
-    print()
+    print('')
     return (printed)
