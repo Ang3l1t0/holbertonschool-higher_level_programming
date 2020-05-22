883a5f85c6a100b462cb3a4079ead4a52aa8f094
Ang3l1t0 <1546@holbertonschool.com>
2020-05-22 00:35:17 -0500

    add docsring

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
-"""print square"""
+"""print_square"""
 
 
 def print_square(size):
+    """print_square
+
+    Arguments:
+        size {int} -- square size
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is lower than 0
+    """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     elif size < 0:
