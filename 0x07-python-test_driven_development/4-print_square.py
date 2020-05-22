7f735157b7ef6d6e134e8f0caddc58939f2e6e83
Ang3l1t0 <1546@holbertonschool.com>
2020-05-22 00:30:41 -0500

    square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""print square"""
+
+
+def print_square(size):
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _i in range(size):
+        print('#' * size)
