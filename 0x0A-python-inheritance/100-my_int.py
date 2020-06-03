48fc274db369ccca3cc65a2448ba88c540138ce0
Ang3l1t0 <1546@holbertonschool.com>
2020-06-02 21:40:11 -0500

    advance

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class MyInt(int):
+    """invert a value
+    """
+
+    def __init__(self, value):
+        """Initialize value
+        """
+        self.value = value
+
+    def __eq__(self, other):
+        """__eq__ represent a==b
+        """
+        return self.value != other
+
+    def __ne__(self, other):
+        """__ne__ represent a != b
+        """
+        return self.value == other
