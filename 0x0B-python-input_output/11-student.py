a6a5f667d2893585a4293f89fd533a2f6b4954bf
Ang3l1t0 <1546@holbertonschool.com>
2020-06-03 19:13:44 -0500

    Student to JSON

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""Student class
+
+Returns:
+    json -- dictionary representation of a Student instance
+"""
+class_to_json = __import__('10-class_to_json').class_to_json
+
+
+class Student:
+    """Class Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initialize method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """to_json method
+        """
+        return class_to_json(self)
