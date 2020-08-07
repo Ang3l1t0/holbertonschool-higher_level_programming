ba75254c2562a1d9839367a60a869888ef84bb38
Ang3l1t0 <1546@holbertonschool.com>
2020-08-07 22:53:02 +0000

    .

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# lists all states from the database
+
+import MySQLdb
+from sys import argv
+
+if __name__=="__main__":
+    db = MySQLdb.connect(host=localhost, port=3306, user=argv[1], 
+                         passwd=argv[2], db=argv[2])
