e67d15be3fddd2de09e4e0bf886d163aceb29a0f
Ang3l1t0 <1546@holbertonschool.com>
2020-08-07 22:58:35 +0000

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,3 +7,4 @@ from sys import argv
 if __name__=="__main__":
     db = MySQLdb.connect(host=localhost, port=3306, user=argv[1], 
                          passwd=argv[2], db=argv[2])
+cur = db.cursor()
