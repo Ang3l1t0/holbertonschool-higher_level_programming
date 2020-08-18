2f49d0fbd4b44f10526af72a2b817da7754c80c2
Ang3l1t0 <1546@holbertonschool.com>
2020-08-18 16:12:21 -0500

    peak 2.0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,16 +6,23 @@ integers
 
 
 def find_peak(list_of_integers):
-    
-    lenght = len(list_of_integers)
-    if lenght == 0:
-        return
-    half = len(list_of_integers) // 2
-    if (half == lenght - 1 or list_of_integers[half] >=
-        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
-                                         list_of_integers[half - 1]):
-        return list_of_integers[half]
 
-    elif half != lenght - 1 and (list_of_integers[half + 1] > list_of_integers[half]):
-        return find_peak(list_of_integers[half + 1:])
-    return find_peak(list_of_integers[:half])
+    if not list_of_integers:
+        return None
+    lenght = len(list_of_integers)
+    if lenght > 1:
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] >= list_of_integers[-2]:
+            return list_of_integers[-1]
+        half = len(list_of_integers) // 2
+        if (half == lenght - 1 or list_of_integers[half] >=
+            list_of_integers[half + 1]) and\
+            (half == 0 or list_of_integers[half] >=
+             list_of_integers[half - 1]):
+            return list_of_integers[half]
+
+        elif half != lenght - 1 and\
+                (list_of_integers[half + 1] > list_of_integers[half]):
+            return find_peak(list_of_integers[half + 1:])
+        return find_peak(list_of_integers[:half])
