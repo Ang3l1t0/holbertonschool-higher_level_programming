1706c5fd783da28c6f01bda0af38c38af2a7bd4e
Ang3l1t0 <1546@holbertonschool.com>
2020-08-18 19:08:14 -0500

    1

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""
-Function that finds a peak in a list of unsorted
-integers
-"""
+"""Finds peak in unsorted list of integers"""
 
 
 def find_peak(list_of_integers):
+    """Finds peak in unsorted list of integers"""
 
     if not list_of_integers:
         return None
-    lenght = len(list_of_integers)
-    if lenght > 1:
+
+    length = len(list_of_integers)
+    half = length // 2
+    if length > 1:
+        if half >= half - 1 and half >= half + 1:
+            return half
+        if (half == length - 1 or list_of_integers[half] >= list_of_integers[half + 1])\
+                and (half == 0 or list_of_integers[half] >= list_of_integers[half - 1]):
+            return list_of_integers[half]
         if list_of_integers[0] >= list_of_integers[1]:
             return list_of_integers[0]
         if list_of_integers[-1] >= list_of_integers[-2]:
             return list_of_integers[-1]
-        half = len(list_of_integers) // 2
-        if (half == lenght - 1 or list_of_integers[half] >=
-            list_of_integers[half + 1]) and\
-            (half == 0 or list_of_integers[half] >=
-             list_of_integers[half - 1]):
-            return list_of_integers[half]
-
-        elif half != lenght - 1 and\
-                (list_of_integers[half + 1] > list_of_integers[half]):
-            return find_peak(list_of_integers[half + 1:])
-        return find_peak(list_of_integers[:half])
+    return find_peak(list_of_integers[half:])
