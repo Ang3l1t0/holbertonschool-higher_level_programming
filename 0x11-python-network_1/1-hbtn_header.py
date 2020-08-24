866290aab143d1fab4d1aa0374c434b00c3c7abd
Ang3l1t0 <1546@holbertonschool.com>
2020-08-24 12:46:08 -0500

    not executed when import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,5 +4,7 @@
 from urllib import request
 from sys import argv
 
-with request.urlopen(argv[1]) as response:
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+
+    with request.urlopen(argv[1]) as response:
+        print(response.getheader('X-Request-Id'))
