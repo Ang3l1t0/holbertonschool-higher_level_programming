e0d6be92834e3783480f44214a6418ab4ced2d6f
Ang3l1t0 <1546@holbertonschool.com>
2020-08-24 15:08:23 -0500

    print()

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    r = requests.post(argv[1], data={'email': argv[2]})
-    print r.text
+    url = argv[1]
+    my_obj = {'email': argv[2]}
+
+    r = requests.post(url, data=my_obj)
+    print(r.text)
