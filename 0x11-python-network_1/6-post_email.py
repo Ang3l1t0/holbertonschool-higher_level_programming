0100a823f23fa2327b4901d46ca35b9ea376932c
Ang3l1t0 <1546@holbertonschool.com>
2020-08-24 15:06:29 -0500

    delete var

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,8 +5,5 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-    my_obj = {'email': argv[2]}
-
-    r = requests.post(url, data=my_obj)
+    r = requests.post(argv[1], data={'email': argv[2]})
     print r.text
