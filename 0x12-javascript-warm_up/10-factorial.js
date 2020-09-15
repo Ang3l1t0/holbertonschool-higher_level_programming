25b9a6fc617297be36016a04d17a5b52033e2b34
Ang3l1t0 <1546@holbertonschool.com>
2020-09-15 18:56:12 -0500

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function rFact (num) {
+  if (num === 0 || isNaN(num)) {
+    return 1;
+  } else {
+    return num * rFact(num - 1);
+  }
+}
+
+const num = Number(process.argv[2]);
+console.log(rFact(num));
