fed881828909827b9ba78a2d80668a1a5a8d7d3c
Ang3l1t0 <1546@holbertonschool.com>
2020-09-22 18:13:24 -0500

    completed task

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// get number of tasks completed by userId
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  !error && console.log(JSON.parse(body).reduce(function (total, current) {
+    current.completed && (total[current.userId] = (total[current.userId] || 0) + 1);
+    return total;
+  }, {}));
+});
