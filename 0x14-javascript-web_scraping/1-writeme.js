f57a47ba3f36746e56e571139a88a10e3a33c919
Ang3l1t0 <1546@holbertonschool.com>
2020-09-22 12:37:17 -0500

    write in js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const path = process.argv[2];
-const content = process.argv[3];
-
-fs.writeFile(path, content, 'utf8', err => {
-  if (err) {
-    console.error(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  err && console.error(err);
 });
